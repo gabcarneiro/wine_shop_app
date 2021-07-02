845e2c62bd4012941a22616b5ab189a430b59680
Gabriel Carneiro <gabcarneiro@outlook.com>
2021-07-01 21:57:07 -0300

    feat: firebase auth facade implementation

--- lib/domain/core/value_object.dart
+++ lib/domain/core/value_object.dart
@@ -2,12 +2,18 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import 'errors.dart';
 import 'failures.dart';
 
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
+
+  /// Throws [UnexpectedValueError] containing the [ValueFailure]
+  T getOrCrash() {
+    return value.fold((l) => throw UnexpectedValueError(l), id);
+  }
 
   bool isValid() => value.isRight();
 
